545fc22c23191451efb52737cd5965fbad69d7e1
Nabiel Alif <nabiel.alif01@gmail.com>
2022-11-07 20:10:28 +0700

    commit brow

--- App.js
+++ App.js
@@ -1,34 +1,28 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "react-native";
-import { Stack } from "./src/router";
-
-import LoginScreen from "./src/screens/login/LoginScreen";
-import { Stack, RegisterScreen }from "./src/screens/login/RegisterScreen";
-
-import MerchantTab  from './src/router';
-import HomeStack from './src/router/user/Stack';
-import Stack from "./src/router/merchant/MerchantStack';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { Stack } from './src/router';
+import LoginScreen from './src/screens/login/LoginScreen';
+import { Stack, RegisterScreen } from './src/screens/login/RegisterScreen';
+import MerchantTab from './src/router';
 import { Provider } from 'react-redux';
-import store from './src/stores/store";
+import store from './src/stores/store';
 
 export default function App() {
   return (
-
     <Provider store={store}>
-    <NavigationContainer>
-      <StatusBar />
-      <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <NavigationContainer>
+        <StatusBar />
+        <Stack.Navigator>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
 
-        <Stack.Screen
-          name="MerchantTab"
-          component={MerchantTab}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen
+            name="MerchantTab"
+            component={MerchantTab}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
