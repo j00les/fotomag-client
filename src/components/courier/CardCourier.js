a9c408af38b9cfb6cef1faf091b97c56e521407b
laktossa <karman.sony@gmail.com>
2022-11-09 16:44:44 +0700

    ayo ayo ayo

---
+++ src/components/courier/CardCourier.js
@@ -0,0 +1,39 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default CardCourier = () => {
+  return (
+    <>
+      <View className="items-center mb-2">
+        <View className="w-[95%] bg-blue-200 rounded-xl p-3">
+          <View>
+            <View className="flex-row justify-between">
+              <Text>Order Id : {id}</Text>
+              <Text>{status}</Text>
+            </View>
+            <Text>Total Lembar : {lembar}</Text>
+            <Text>Warna: {warna}</Text>
+            <Text>Jilid : {jilid}</Text>
+            <Text>Total Harga : {harga}</Text>
+            <Text>Alamat : {alamat}</Text>
+          </View>
+          <View className="w-full items-center">
+            <View className="w-[95%] flex-row mt-[4%] justify-between">
+              <TouchableOpacity
+                onPress={() => reject(id)}
+                className="w-[45%] bg-red-500"
+              >
+                <Text className="text-center">Tolak</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => progress(id)}
+                className="w-[45%] bg-green-500 "
+              >
+                <Text className="text-center">Terima</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+};
---
+++ src/screens/courier/HomeCourier.js
@@ -0,0 +1,63 @@
+import { FlatList, Text, View } from "react-native";
+
+export default HomeCourier = () => {
+  return (
+    <>
+      <View className="flex-1 items-center ">
+        <View className="h-[16%] w-full bg-red-300 rounded-b-3xl ">
+          <View className="flex-row h-[50%] items-center justify-center mt-[3%] px-[4%]">
+            <View className="">
+              <Text className="text-2xl font-bold">
+                Selamat datang, Your Name
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View className="bg-white h-[5%]" />
+        <View className="h-[85%] w-full bg-red-300 rounded-t-3xl items-center ">
+          <View className="bg-white w-[95%] h-full mt-[12%] rounded-t-3xl">
+            <View className="bg-yellow-200  rounded-t-3xl h-[7%] rounded-b-3xl justify-center items-center">
+              <Text>Orderan</Text>
+            </View>
+            <View className="bg-white h-[1%]" />
+            <View className="bg-yellow-200 h-[78%] rounded-t-3xl">
+              <View className="p-4 mt-3 h-full">
+                <FlatList
+                //   key={pending.id}
+                //   data={pending}
+                //   renderItem={({ item }) => (
+                //     <Card
+                //       nama="home"
+                //       id={item.id}
+                //       warna={item.colorVariant}
+                //       lembar={item.totalPages}
+                //       jilid={item.isJilid}
+                //       harga={item.totalPrice}
+                //       alamat={item.address}
+                //       status={item.status}
+                //     />
+                //   )}
+                //   refreshing={refresh}
+                //   onRefresh={() => {
+                //     setRefresh(true),
+                //       setTimeout(() => {
+                //         console.log("wow");
+                //       }, 5000),
+                //       setRefresh(false);
+                //   }}
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+
+        <View className="h-[15%] w-[80%] bg-white border top-[11%] absolute rounded-xl ">
+          <View className="h-full bg-slate-400 items-center justify-center rounded-xl">
+            <Text className="font-bold text-lg ">Pendapatan</Text>
+            <Text className="font-bold text-4xl ">Rp. 2.000.000</Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+};
